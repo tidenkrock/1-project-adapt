473c57fff0f02bc2462745cb84b9d621580717b1
TIDen <tidenkrock@gmail.com>
2025-03-04 23:46:57 +0500

    Первый коммит

---
+++ main.js
@@ -0,0 +1,25 @@
+var number = 10;
+var a = 5;
+var b = 10;
+var c = 15;
+
+// if(number == 5) {
+//     console.log("5 is equal to 5");
+//     console.log(number);
+// }
+// else if(number != 10) {
+//     console.log("nigga")
+// }
+// else {
+//     console.log(a + b * c)
+// }
+
+// if(number + a == c) {
+//     console.log("correct")
+// }
+
+for(var i = 0; i <= 200; i /= 2) {
+    console.log(i)
+    console.log(i * a + b + c)
+    return
+}
